4b18034a1acd365d0aac6e1a58faa4e7f24ff095
Sanjee <sanjee.yogeswaran@gmail.com>
2017-02-27 16:58:08 -0600

    port change

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-app.listen(3000, function () {
-
-})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
